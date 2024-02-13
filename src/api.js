2a10ff942e76a86335e7958b83ff6df68a792f72
ukhan57 <ukhan57@myseneca.ca>
2024-02-13 18:36:14 -0500

    Fixing bugs

--- src/api.js
+++ src/api.js
@@ -50,7 +50,7 @@ export async function postUserFragment(user, fragmentText) {
       throw new Error(`${res.status} ${res.statusText}`);
     }
     const data = await res.json();
-    const location = res.headers.get('Location');
+    const location = res.headers.get.location;
     console.log('Location: ', location);
     console.log('Successfully posted fragment data', { data });
     return { data, location };
